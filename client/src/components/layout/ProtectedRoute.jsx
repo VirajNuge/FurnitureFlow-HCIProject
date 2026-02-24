698c06bcbb3b564c499a1ff251f2e8202d2d26da
semalhewage <semalhewage31@gmail.com>
2026-02-24 12:00:00 +0530

    feat(client): create ProtectedRoute component for auth-guarded pages

---
+++ client/src/components/layout/ProtectedRoute.jsx
@@ -0,0 +1,9 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import useAuthStore from '../../store/authStore';
+
+const ProtectedRoute = () => {
+  const { user } = useAuthStore();
+  return user ? <Outlet /> : <Navigate to="/login" replace />;
+};
+
+export default ProtectedRoute;
