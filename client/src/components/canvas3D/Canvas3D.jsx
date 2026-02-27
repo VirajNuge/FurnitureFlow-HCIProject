b23cdc4afebf446398c66f0713f275c6813031f4
VirajNuge <vnugekotuwa12@gmail.com>
2026-02-27 16:30:00 +0530

    feat(client): add ambient and directional lighting to 3D scene

--- client/src/components/canvas3D/Canvas3D.jsx
+++ client/src/components/canvas3D/Canvas3D.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
 
-const Canvas3D = () => {
+const Canvas3D = ({ children }) => {
   return (
-    <Canvas style={{ width: '100%', height: '100%' }}>
-      <ambientLight intensity={0.5} />
-      <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="orange" />
-      </mesh>
+    <Canvas
+      style={{ width: '100%', height: '100%' }}
+      camera={{ position: [0, 8, 12], fov: 50 }}
+    >
+      <ambientLight intensity={0.6} />
+      <directionalLight
+        position={[10, 20, 10]}
+        intensity={1.0}
+        castShadow
+        shadow-mapSize-width={2048}
+        shadow-mapSize-height={2048}
+      />
+      <pointLight position={[-10, 10, -10]} intensity={0.3} />
+      {children}
     </Canvas>
   );
 };
